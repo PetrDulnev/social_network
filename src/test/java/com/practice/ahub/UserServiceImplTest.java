958b12fef039a9a09f4a07589947e744086cdd1b
PetrDulnev <dulnev.fortech@gmail.com>
2024-04-17 16:04:45 +0300

    Add ControllerAdvice handler

---
+++ src/test/java/com/practice/ahub/UserServiceImplTest.java
@@ -0,0 +1,21 @@
+package com.practice.ahub;
+
+import org.junit.jupiter.api.Test;
+
+public class UserServiceImplTest {
+
+    @Test
+    public void createUser() {
+
+    }
+
+    @Test
+    public void loadUserByUsername() {
+
+    }
+
+    @Test
+    public void login() {
+
+    }
+}
