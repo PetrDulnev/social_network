448d145428309da7d698ac53b414eb0551578816
PetrDulnev <dulnev.fortech@gmail.com>
2024-05-03 14:49:19 +0300

    add exception!

--- src/main/java/com/practice/ahub/conroller/PostController.java
+++
@@ -1,8 +0,0 @@
-package com.practice.ahub.conroller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class PostController {
-
-}
--- src/main/java/com/practice/ahub/service/FileModelService.java
+++ src/main/java/com/practice/ahub/service/FileModelService.java
@@ -1,5 +1,6 @@
 package com.practice.ahub.service;
 
+import com.practice.ahub.exception.CustomException;
 import com.practice.ahub.model.AccessFileExtension;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,15 +10,13 @@ import org.springframework.web.multipart.MultipartFile;
 @RequiredArgsConstructor
 public class FileModelService {
 
-    public boolean isValidFile(MultipartFile file) {
+    public void isValidFile(MultipartFile file) {
         if (file != null && file.getOriginalFilename() != null && !file.getOriginalFilename().isEmpty()){
             try {
                 AccessFileExtension.valueOf(file.getContentType().split("/")[1].toUpperCase());
-                return true;
             }catch (IllegalArgumentException e){
-                return false;
+                throw new CustomException("unsupported file format");
             }
         }
-        return false;
     }
 }
